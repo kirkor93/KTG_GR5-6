5399ed0e06d6ad4d1094660edbb3214d5a07bbcc
vansten <krzysztof.stengert@hotmail.com>
2014-04-07 13:12:24 +0200

    System monetek zrobiony, HUD, zbieranie i prefab.

---
+++ Game/Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	//public variables
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward, Time.deltaTime * rotationSpeed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("I'm here");
+		if (other.gameObject.tag == "Player")
+		{
+			Inventory.CoinCount++;
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+	public static int CoinCount;
+	public GUIText coins;
+
+	// Use this for initialization
+	void Start () {
+		CoinCount = 0;
+		coins.text = CoinCount.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coins.text = CoinCount.ToString ();
+	}
+}
