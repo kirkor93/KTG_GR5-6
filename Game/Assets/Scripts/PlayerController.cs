6e64c6fc34205ab675f060e4edbbfe35c972e828
NethRaiden <jmikicin@gmail.com>
2014-04-08 13:52:09 +0200

    Eksperyment z buttonami do sterowania

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float SideSpeed;
     public float SideOffset;
+    public GUIStyle invisibleButton;
     Vector3 newPos;
     int CurrentLane;
     int SideDirection;
     bool SwitchingLanes;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         CurrentLane = 0;
         SwitchingLanes = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    void OnGUI()
+    {
+        if (!SwitchingLanes)
+        {
+            if (GUI.Button(new Rect(0, 0, Screen.width / 2, Screen.height), "Left", invisibleButton) && CurrentLane != 1)
+            {
+                CurrentLane++;
+                SideDirection = 1;
+                newPos.x = transform.position.x + SideOffset;
+                newPos.y = transform.position.y;
+                newPos.z = transform.position.z;
+                SwitchingLanes = true;
+            }
+            if (GUI.Button(new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height), "Right", invisibleButton) && CurrentLane != -1)
+            {
+                CurrentLane--;
+                SideDirection = -1;
+                newPos.x = transform.position.x - SideOffset;
+                newPos.y = transform.position.y;
+                newPos.z = transform.position.z;
+                SwitchingLanes = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (SwitchingLanes)
         {
             float step = SideSpeed * Time.deltaTime;
 
             transform.position = Vector3.MoveTowards(transform.position, newPos, step);
-            transform.Rotate(transform.forward, 2*(transform.position.x - newPos.x + SideDirection * SideOffset/2) );
-            
+            transform.Rotate(transform.forward, 2 * (transform.position.x - newPos.x + SideDirection * SideOffset / 2));
+
             if (transform.position == newPos)
             {
                 transform.rotation = Quaternion.identity;
@@ -52,5 +81,5 @@ public class PlayerController : MonoBehaviour {
                 SwitchingLanes = true;
             }
         }
-	}
+    }
 }
