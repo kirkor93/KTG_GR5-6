2a75928ae31e69ba0a857ac013ebdc122358ea85
vansten <krzysztof.stengert@hotmail.com>
2014-04-10 19:41:54 +0200

    Monetki zrobione, pojawiaja sie losowo. Ewentualnie mozna pomyslec nad tym by tak fajnie wyskakiwaly.

---
+++ Game/Assets/Scripts/TrainController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainController : MonoBehaviour {
+
+	public GameObject coinPrefab;
+	public float coinProbability = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(coinProbability > 1.0f)
+		{
+			coinProbability--;
+		}
+		while(coinProbability < 0.0f)
+		{
+			coinProbability++;
+		}
+
+		int rndNumber = Random.Range (0, 10000);
+		if((float)rndNumber/10000 >= coinProbability)
+		{
+			int lane = Random.Range (0,3);
+			lane--;
+			Vector3 coinPosition = this.transform.position;
+			coinPosition.x = lane*2.5f;
+			Instantiate(coinPrefab, coinPosition, coinPrefab.transform.rotation);
+		}
+
+	}
+}
