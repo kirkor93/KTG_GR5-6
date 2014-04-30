1dea4610741575032d00c358dfd92f1e00835e2e
NethRaiden <jmikicin@gmail.com>
2014-04-30 14:59:09 +0200

    whiskey i tnt tweaks

---
+++ Game/Assets/Scripts/Whiskey.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whiskey : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            other.gameObject.SendMessage("WhiskeyInAJar", 6.0f);
+        }
+    }
+}
